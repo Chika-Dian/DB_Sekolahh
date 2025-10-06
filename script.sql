a0137d8d464bf42e15ac7296d03660cdad2abc51
Chika Dian Nurcahyani <emailkamu@gmail.com>
2025-10-06 22:32:42 +0700

    mengisi...tabel.

--- script.sql
+++ script.sql
@@ -17,3 +17,32 @@ create table nilai (
 );
 select * from nilai;
 
+-- mengisi value dari tabel siswa
+insert into siswa (nama, umur, jurusan)
+values 
+('Andi', 15, 'IPA'),
+('Budi', 16, 'IPS'),
+('Dika', 16, 'IPA'),
+('Dian', 17, 'IPS'),
+('Chika', 18, 'TKJ'),
+('Ian', 19, 'Multimedia'),
+('Brilian', 19, 'Multimedia'),
+('Yani', 19, 'Multimedia');
+select * from siswa;
+
+-- mengisi value dari tabel nilai
+insert into nilai (id_siswa, mata_pelajaran, nilai)
+values
+(1, 'Matematika', 85),
+(1, 'Matematika Peminatan', 80),
+(2, 'Bahasa Inggris', 90),
+(2, 'Sejarah', 90),
+(3, 'TIK', 85),
+(4, 'IPA', 70),
+(5, 'Pemrograman', 75),
+(5, 'Jaringan', 70),
+(6, 'Bahasa Indonesia', 80),
+(7, 'Manajemen', 100),
+(8, 'Bahasa Mandarin', 60);
+select * from nilai;
+
